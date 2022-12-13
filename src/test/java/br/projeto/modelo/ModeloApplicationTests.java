ac1c022c74081e366318d612eaa1e84d0a302ca2
Arquitetura <paulo.roberto@unifor.br>
2022-12-13 17:24:34 -0300

    Init project

---
+++ src/test/java/br/projeto/modelo/ModeloApplicationTests.java
@@ -0,0 +1,12 @@
+package br.projeto.modelo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
